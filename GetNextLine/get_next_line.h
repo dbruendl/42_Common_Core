70d4871d76509fb7815baa25f626f6ecfb74770f
Daniel Bründl <dbruendl@hotmail.com>
2023-10-06 14:17:26 +0200

    now with right function in header

--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -21,7 +21,7 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(const char *string, int searchedChar );
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t elementCount, size_t elementSize);
